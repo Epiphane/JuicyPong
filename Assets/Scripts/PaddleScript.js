c2c90ec3ed04184d7335bfd8cf7fccf5c7f2a5f3
Epiphane <exyphnos@gmail.com>
2015-08-22 20:20:02 -0700

    First "Scene"

---
+++ Assets/Scripts/PaddleScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var speed : int = 0.5;
+public var player : int = 1;
+
+function Start () {
+	
+}
+
+function FixedUpdate () {
+	transform.position.y += Input.GetAxisRaw("P" + player + " Vertical") * Time.deltaTime * speed;
+}
