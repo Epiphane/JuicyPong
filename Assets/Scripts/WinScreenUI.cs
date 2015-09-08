a19cad3747f628e20275325a60a0123c74ca9dc9
elliotfiske <elliotfiske@gmail.com>
2015-09-08 13:32:03 -0700

    Experience Screen Basic
    da basicest of basic

--- Assets/Scripts/WinScreenUI.cs
+++ Assets/Scripts/WinScreenUI.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class WinScreenText : MonoBehaviour {
+public class WinScreenUI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
